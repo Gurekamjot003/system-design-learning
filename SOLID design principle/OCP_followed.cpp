5e64653542b54dabc0fe973175140f1978562f23
Gurekamjot003 <aroragurekam03@gmail.com>
2025-05-19 01:37:24 +0530

    Open Close Principle OCP in SOLID design principles

---
+++ SOLID design principle/OCP_followed.cpp
@@ -0,0 +1,105 @@
+//OPEN CLOSE PRINCIPLE
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Product{
+public:
+    string name;
+    double price;
+
+    Product(string name, double price){
+        this->name = name;
+        this->price = price;
+    }
+};
+
+class ShoppingCart{
+private:
+    vector<Product*> products; // 1 shopping cart can have multiple products 1 to many relationship
+
+public:
+    void add_product(Product* p){
+        products.push_back(p);
+    }
+
+    const vector<Product*>& get_products(){
+        return products;
+    }
+    // responsibility 1
+    double calculate_total(){
+        double ans = 0;
+        for(auto& p: products){
+            ans += p->price;
+        }
+        return ans;
+    }
+
+};
+
+class ShoppingCartPrinter{
+    private:
+    ShoppingCart* cart; // has a relationship
+
+    public:
+    ShoppingCartPrinter(ShoppingCart* cart){
+        this->cart = cart;
+    }
+
+    void print_invoice(){
+        cout<<"Shopping Cart Invoice: "<<endl;
+        for(auto& p: cart->get_products()){
+            cout<<p->name<<" - $"<<p->price<<endl;
+        }
+        cout<<"Total: $"<<cart->calculate_total()<<endl;
+    }
+    
+};
+
+//abstract class
+class persistence{
+    private: 
+    ShoppingCart* cart;
+
+    public:
+    virtual void save(ShoppingCart* cart) = 0;
+};
+
+class SQL_persistance: public persistence{
+    public:
+    void save(ShoppingCart* cart) override{
+        cout<<"Saving to SQL database..."<<endl;
+    }
+};
+
+class mongo_persistance: public persistence{
+    public:
+    void save(ShoppingCart* cart) override{
+        cout<<"Saving to mongo database..."<<endl;
+    }
+};
+
+class file_persistance: public persistence{
+    public:
+    void save(ShoppingCart* cart) override{
+        cout<<"Saving to file..."<<endl;
+    }
+};
+
+int main(){
+    ShoppingCart *cart = new ShoppingCart();
+    
+    cart->add_product(new Product("Monitor", 6000));
+    cart->add_product(new Product("Mouse", 500));
+
+    ShoppingCartPrinter* printer = new ShoppingCartPrinter(cart);
+    printer->print_invoice();
+
+    persistence* db = new SQL_persistance();
+    persistence* mongo = new mongo_persistance();
+    persistence* file = new file_persistance();
+
+    db->save(cart);
+    mongo->save(cart);
+    file->save(cart);
+}
---
+++ SOLID design principle/OCP_violated.cpp
@@ -0,0 +1,94 @@
+//OPEN CLOSE PRINCIPLE
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Product{
+public:
+    string name;
+    double price;
+
+    Product(string name, double price){
+        this->name = name;
+        this->price = price;
+    }
+};
+
+class ShoppingCart{
+private:
+    vector<Product*> products; // 1 shopping cart can have multiple products 1 to many relationship
+
+public:
+    void add_product(Product* p){
+        products.push_back(p);
+    }
+
+    const vector<Product*>& get_products(){
+        return products;
+    }
+    // responsibility 1
+    double calculate_total(){
+        double ans = 0;
+        for(auto& p: products){
+            ans += p->price;
+        }
+        return ans;
+    }
+
+};
+
+class ShoppingCartPrinter{
+    private:
+    ShoppingCart* cart; // has a relationship
+
+    public:
+    ShoppingCartPrinter(ShoppingCart* cart){
+        this->cart = cart;
+    }
+
+    void print_invoice(){
+        cout<<"Shopping Cart Invoice: "<<endl;
+        for(auto& p: cart->get_products()){
+            cout<<p->name<<" - $"<<p->price<<endl;
+        }
+        cout<<"Total: $"<<cart->calculate_total()<<endl;
+    }
+    
+};
+
+class ShoppingCartStorage{
+    private:
+    ShoppingCart* cart; // has a relationship
+
+    public:
+    ShoppingCartStorage(ShoppingCart* cart){
+        this->cart = cart;
+    }
+
+    void save_to_SQL_database(){
+        cout<<"Saving to SQL database..."<<endl;
+    }
+
+    void save_to_mongo_database(){
+        cout<<"Saving to Mongo database..."<<endl;
+    }
+
+    void save_to_file(){
+        cout<<"Saving to file..."<<endl;
+    }
+
+    
+};
+
+int main(){
+    ShoppingCart *cart = new ShoppingCart();
+    
+    cart->add_product(new Product("Monitor", 6000));
+    cart->add_product(new Product("Mouse", 500));
+
+    ShoppingCartPrinter* printer = new ShoppingCartPrinter(cart);
+    printer->print_invoice();
+
+    ShoppingCartStorage* db = new ShoppingCartStorage(cart);
+    db->save_to_SQL_database();
+}
